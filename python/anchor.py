ac447660fee7ec2fe65d6ebaecb16a3b4cb27f25
刘启荣 <sixizi@gmail.com>
2013-10-15 16:29:42 +0800

    test anchor

--- python/anchor.py
+++ python/anchor.py
@@ -4,9 +4,10 @@ import sys
 
 def handle(line):
     ''''''
-    line = line.strip("*").strip()
+    line = line.split("]")[0].split("[")[1] #line.strip("*").strip()
     #return "* [{0}](#{0})<a> name='{0}' id='{0}'</a>  ".format(line)
-    return "* [{0}](id:statvar_{0})  ".format(line)
+    an = "statvar_" + line
+    return "* <a name='{0}' href='#{0}'>{1}</a>  ".format(an, line)
     
 if __name__ == "__main__":
     fn = sys.argv[1]
